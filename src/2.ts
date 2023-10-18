111fadc2d6bcbc6cba1d8792ab43d9fd40eb06f7
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2023-10-18 18:42:16 +0300

    1

--- src/2.ts
+++ src/2.ts
@@ -3,9 +3,9 @@
 
   Клас Employee повинен включати:
 
-  властивість name, яка буде доступна всім.
-  властивість department, яка буде доступна лише всередині класу Employee.
-  salary, яке буде доступне лише всередині класу Employee та його підкласів.
+  властивість name, яка буде доступна всім. (public)
+  властивість department, яка буде доступна лише всередині класу Employee. (private)
+  salary, яке буде доступне лише всередині класу Employee та його підкласів. (protected)
 
 
   Клас Manager повинен бути підклас класу Employee
@@ -16,24 +16,33 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public  name: string;
+  private department: string;
+  protected salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor(name: string,  department: string, salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
   }
 
+
+
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  
+  }
+
+}
+
+
+class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary += 10000;
   }
 }
 
-class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
-}
 
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,64 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(door: boolean, key: Key) {
+      this.door = door;
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door && this.key.getSignature() === person.getKey().getSignature()) {
+        this.tenants.push(person);
+        console.log('Person came in.');
+      } else {
+        console.log('Person can\'t come in.');
+      }
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('Door opened.');
+      } else {
+        console.log('Door remains closed.');
+      }
+    }
+  }
+  
+  const key = new Key();
+  const house = new MyHouse(false, key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
